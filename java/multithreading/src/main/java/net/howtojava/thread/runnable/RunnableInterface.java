d584adae4ecdb1d8495152fea8a81ce6cac7e232
Omer Gillani <sogillani@gmail.com>
2024-07-04 16:20:08 +0200

    add Runnable interface

---
+++ java/multithreading/src/main/java/net/howtojava/thread/runnable/RunnableInterface.java
@@ -0,0 +1,26 @@
+package net.howtojava.thread.runnable;
+
+class Runner implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Count: " + i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+public class RunnableInterface {
+    public static void main(String[] args) {
+      Thread threadOne = new Thread(new Runner());
+      threadOne.start();
+
+      Thread threadTwo = new Thread(new Runner());
+      threadTwo.start();
+    }
+}
